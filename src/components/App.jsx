bed7282ea8c7bed4f1ab98514b56578e245eefea
himanshu76200 <himanshu76200@gmail.com>
2021-03-27 10:43:13 +0530

    added meterial UI and Icons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,42 +3,39 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
- 
 
 function App() {
-
-  const[notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState([]);
 
   function addNote(newNote) {
     setNotes(prevNotes => {
-      return[...prevNotes, newNote]
-    })
+      return [...prevNotes, newNote];
+    });
   }
 
   function deleteNote(id) {
     setNotes(prevNotes => {
-      return(
-        prevNotes.filter((noteItem, index) => {
+      return prevNotes.filter((noteItem, index) => {
         return index !== id;
-        })
-      );
-    })
+      });
+    });
   }
 
   return (
     <div>
       <Header />
-      <CreateArea addNote={addNote}/>
+      <CreateArea onAdd={addNote} />
       {notes.map((noteItem, index) => {
-        return <Note
-          key={index}
-          id={index}
-          title={noteItem.title}
-          content={noteItem.content}
-          onDelete={deleteNote}
-        />
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
       })}
-      
       <Footer />
     </div>
   );
